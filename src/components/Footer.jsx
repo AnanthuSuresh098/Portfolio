33f82138db75ce9e2c15dc9d737fefe5a6478e33
AnanthuSuresh098 <93374262+AnanthuSuresh098@users.noreply.github.com>
2022-02-12 19:01:41 +0530

    updated code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,26 @@
 import React from "react";
+// import CallIcon from "@mui/icons-material/Call";
 
 const Footer = () => {
   return (
     <div>
       <br></br>
       <hr></hr>
+      <div id="get-intouch">
+        <div id="getintouch-head">GET IN TOUCH</div>
+        <div id="getintouch-txt">
+          I'm looking forward for new opportunities in full-stack web
+          development.
+        </div>
+        <div>
+        {/* <div>{CallIcon}</div> */}
+        </div>
+      </div>
       <p className="footer-text">
         <br></br>
         <small>All rights reserved © 2021 Tejaswi</small>
         <br></br>
-        <small>Illustrations by undraw</small>
+        <small>Illustrations by undraw.co</small>
       </p>
     </div>
   );
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -12,66 +12,129 @@ import { faCss3Alt, faDocker, faHtml5, faJs, faReact ,faNodeJs } from '@fortawes
 
 const Skills = ({darkMode}) => {
     return (
-        <div data-theme={darkMode ? "dark" : "light"}>
-            <br></br>
-            <Slide>
-            <Container>
-                <Row>
-                <Col lg={4} xs={8}>
-                    <h1 className="display-3 skill-h">I can work <p className="with">with</p> </h1>
-                    <img src={darkMode? skillsDark : skillsLight} alt="skills" className="skills-img img-fluid animated"/>
-                </Col>
-                <Col lg={2} xs={4}>
-                    <br></br>
-                    <Card className="nbcard" id="skill-sec">
-                    <Button variant={darkMode ? "outline-light" : "outline-dark"} className="skills-btn">
-                    <FontAwesomeIcon className="skill" size='2x' icon={faHtml5}/>
+      <div data-theme={darkMode ? "dark" : "light"}>
+        <br></br>
+        <Slide>
+          <Container>
+            <Row>
+              <Col lg={4} xs={8}>
+                <h1 className="display-3 skill-h">
+                  I can work <p className="with">with</p>{" "}
+                </h1>
+                <img
+                  src={darkMode ? skillsDark : skillsLight}
+                  alt="skills"
+                  className="skills-img img-fluid animated"
+                />
+              </Col>
+              <Col lg={2} xs={4}>
+                <br></br>
+                <Card className="nbcard" id="skill-sec">
+                  <div id="html-wrap">
+                    <Button
+                      variant={darkMode ? "outline-light" : "outline-dark"}
+                      className="skills-btn"
+                      id="html-btn"
+                    >
+                      <FontAwesomeIcon
+                        className="skill"
+                        size="2x"
+                        icon={faHtml5}
+                      />
                     </Button>
-                    <Button variant={darkMode ? "outline-light" : "outline-dark"} className="skills-btn">
-                    <FontAwesomeIcon className="skill" size='2x' icon={faCss3Alt}/>
+                    <div id="html-pop">HTML</div>
+                  </div>
+                  <div id="css-wrap">
+                    <Button
+                      variant={darkMode ? "outline-light" : "outline-dark"}
+                      className="skills-btn"
+                      id="css-btn"
+                    >
+                      <FontAwesomeIcon
+                        className="skill"
+                        size="2x"
+                        icon={faCss3Alt}
+                      />
                     </Button>
-                    <Button variant={darkMode ? "outline-light" : "outline-dark"} className="skills-btn">
-                    <FontAwesomeIcon className="skill" size='2x' icon={faJs}/>
+                    <div id="css-pop">CSS</div>
+                  </div>
+                  <div id="js-wrap">
+                    <Button
+                      variant={darkMode ? "outline-light" : "outline-dark"}
+                      className="skills-btn"
+                      id="js-btn"
+                    >
+                      <FontAwesomeIcon
+                        className="skill"
+                        size="2x"
+                        icon={faJs}
+                      />
                     </Button>
-                    <Button variant={darkMode ? "outline-light" : "outline-dark"} className="skills-btn">
-                    <FontAwesomeIcon className="skill" size='2x' icon={faReact}/>
+                    <div id="js-pop">JAVASCRIPT</div>
+                  </div>
+                  <div id="react-wrap">
+                    <Button
+                      variant={darkMode ? "outline-light" : "outline-dark"}
+                      className="skills-btn"
+                      id="react-btn"
+                    >
+                      <FontAwesomeIcon
+                        className="skill"
+                        size="2x"
+                        icon={faReact}
+                      />
                     </Button>
-                    <Button variant={darkMode ? "outline-light" : "outline-dark"} className="skills-btn">
-                    <FontAwesomeIcon className="skill" size='2x' icon={faNodeJs}/>
+                    <div id="react-pop">REACT</div>
+                  </div>
+                  <div id="node-wrap">
+                    <Button
+                      variant={darkMode ? "outline-light" : "outline-dark"}
+                      className="skills-btn"
+                      id="node-btn"
+                    >
+                      <FontAwesomeIcon
+                        className="skill"
+                        size="2x"
+                        icon={faNodeJs}
+                      />
                     </Button>
-                    {/* <Button variant={darkMode ? "outline-light" : "outline-dark"} className="skills-btn">
+                    <div id="node-pop">NODE</div>
+                  </div>
+                  {/* <Button variant={darkMode ? "outline-light" : "outline-dark"} className="skills-btn">
                     <p align="center" className="skill-txt">C</p>
                     </Button> */}
-                    </Card>
-                </Col>
-                <Col lg={6} xs={12}>
-                    <br></br>
-                    {/* <Card className="skill-nbcard">
+                </Card>
+              </Col>
+              <Col lg={6} xs={12}>
+                <br></br>
+                {/* <Card className="skill-nbcard">
                         <Card.Body>
                             <Card.Title>Programming Languages</Card.Title>
                             <Card.Text>C Programming, C++, GNU Octave, Python, JavaScript</Card.Text>
                         </Card.Body>
                     </Card> */}
-                    <Card className="skill-nbcard">
-                        <Card.Body >
-                            <Card.Title>Technologies</Card.Title>
-                            <Card.Text>HTML , CSS , JS , REACT , MONGODB , REDUX , NODE , EXPRESS</Card.Text>
-                        </Card.Body>
-                    </Card>
-                    {/* <Card className="skill-nbcard">
+                <Card className="skill-nbcard">
+                  <Card.Body>
+                    <Card.Title>Technologies</Card.Title>
+                    <Card.Text>
+                      HTML , CSS , JS , REACT , MONGODB , REDUX , NODE , EXPRESS
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+                {/* <Card className="skill-nbcard">
                         <Card.Body >
                             <Card.Title>Others</Card.Title>
                             <Card.Text>MS office, Graphic Designing</Card.Text>
                         </Card.Body>
                     </Card> */}
-                </Col>
-                </Row>
-                <br></br>
-                <hr></hr>
-            </Container>
-            </Slide>
-        </div>
-    )
+              </Col>
+            </Row>
+            <br></br>
+            <hr></hr>
+          </Container>
+        </Slide>
+      </div>
+    );
 }
 
 export default Skills
